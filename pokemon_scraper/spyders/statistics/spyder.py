b1efa68997ed67d3e94a79a20383db8a23182426
sergioaisolutions <sergioaisolutions@gmail.com>
2021-08-22 20:35:33 +0200

    get url - accept cookies - get pokemon names
    New features implemented in the statistics spider: get the url of the Pokémon battle simulation page, accept the cookies button and get all the names of all the Pokémon available.

--- pokemon_scraper/spyders/statistics/spyder.py
+++ pokemon_scraper/spyders/statistics/spyder.py
@@ -1,5 +1,27 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def statistics_driver(driver, binary):
-    
     return webdriver.Firefox(executable_path = driver, firefox_binary = binary)
+
+def get_url(driver, url):
+    return driver.get(url)
+
+def accept_cookies(driver, css_selector):
+    return WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, css_selector))).click()
+
+def get_pokemon_names(driver, css_selector_to_wait, css_selector_dropdown):
+
+    pokemons_list = list()
+
+    pokemon_selector = driver.find_element_by_css_selector(css_selector_dropdown)
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, css_selector_to_wait)))
+    select_pokemon = Select(pokemon_selector)
+
+    for pokemon in select_pokemon.options:
+        pokemons_list.append(pokemon.text)
+
+    return pokemons_list
--- pokemon_scraper/spyders/statistics/statistics.py
+++ pokemon_scraper/spyders/statistics/statistics.py
@@ -1,5 +1,9 @@
-from spyder import statistics_driver
-from resources import geckodriver, firefox_binary
+from spyder import *
+from resources import *
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
 statistics_spyder = statistics_driver(geckodriver, binary = FirefoxBinary(firefox_binary))
+
+get_url(statistics_spyder, pokemon_url)
+accept_cookies(statistics_spyder, accept_cookies_css_selector)
+pokemon_names = get_pokemon_names(statistics_spyder, dropdown_last_element_css_selector, dropdown_css_selector)
