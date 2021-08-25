00cb9700d9744229188940b654827a747f6850a8
sergioaisolutions <sergioaisolutions@gmail.com>
2021-08-25 17:43:46 +0200

    combat points, attack and defense
    Storing combat points, attack and defense statistics in variables.

--- pokemon_scraper/spyders/statistics/spyder.py
+++ pokemon_scraper/spyders/statistics/spyder.py
@@ -27,10 +27,14 @@ def pokemon_names(driver, css_selector_to_wait, css_selector_dropdown):
 
     return pokemons_list
 
-def pokemon_statistics(driver, css_selector_search_engine, pokemon_names):
+def pokemon_statistics(driver, css_selector_search_engine, pokemon_names,
+                       cp_css_selector, atk_css_selector, def_css_selector):
 
     search_engine = driver.find_element_by_css_selector(css_selector_search_engine)
 
     for pokemon_name in pokemon_names:
         search_engine.send_keys(pokemon_name)
+        combat_points = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, cp_css_selector)))
+        attack = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, atk_css_selector)))
+        defense = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, def_css_selector)))
         search_engine.clear()
--- pokemon_scraper/spyders/statistics/statistics.py
+++ pokemon_scraper/spyders/statistics/statistics.py
@@ -7,4 +7,6 @@ statistics_spyder = statistics_driver(geckodriver, binary = FirefoxBinary(firefo
 get_url(statistics_spyder, pokemon_url)
 accept_cookies(statistics_spyder, accept_cookies_css_selector)
 pokemon_names = pokemon_names(statistics_spyder, dropdown_last_element_css_selector, dropdown_css_selector)
-pokemon_statistics(statistics_spyder, search_field_css_selector, pokemon_names)
+
+pokemon_statistics(statistics_spyder, search_field_css_selector, pokemon_names,
+                   combat_points_css_selector, attack_css_selector, defense_css_selector)
